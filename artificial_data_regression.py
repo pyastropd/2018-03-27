f437fe87d237a6f19a881409b2af2b8e5a1cb34b
Mario Pasquato <mario.pasquato@gmail.com>
2018-03-26 22:33:06 +0200

    comparison of training on different epochs

--- artificial_data_regression.py
+++ artificial_data_regression.py
@@ -21,11 +21,25 @@ nn.compile(loss='mean_squared_error', optimizer='sgd') #minimize mean squared er
 #Train the net (sees the data 100 times)
 nn.fit(train_x, train_y, epochs=100, batch_size=10)
 
+#Generate more data to test the performance
 test_x = np.random.rand(1000, 2)
 test_y = test_x[:,1]*(1.0 - test_x[:,0])
 
+#predict on the test data
 pred_y = nn.predict(test_x)
 pred_y.shape = [pred_y.size,]
 
+#plot the predictions as a function of the "real" values
+plt.scatter(test_y, pred_y)
+plt.show()
+
+#Train the net on more epochs (sees the data 500 times)
+nn.fit(train_x, train_y, epochs=500, batch_size=10)
+
+#predict on the test data with the more trained net
+pred_y = nn.predict(test_x)
+pred_y.shape = [pred_y.size,]
+
+#plot the predictions as a function of the "real" values
 plt.scatter(test_y, pred_y)
 plt.show()
